9dcf068b6c7ca8185eb2a2f4069c3078120ebd67
Timoteo Ruiz de Arcaute <timoteorda@gmail.com>
2021-12-29 14:13:35 -0300

    Ajustes tienda

--- js/clase.js
+++ js/clase.js
@@ -1,10 +1,11 @@
 class Producto {
-    constructor(tipo, modelo, precio, stock, img){
+    constructor(tipo, modelo, precio, stock, img, indice){
         this.tipo = tipo
         this.modelo = modelo
         this.precio = precio
         this.stock = stock
         this.img = img
+        this.indice = indice
         this.cant = 1
     }
 }
--- js/javascript.js
+++ js/javascript.js
@@ -43,6 +43,7 @@ $( () =>{
     $(".tituloTienda").removeClass("tituloModoOscuro")
     $(".botonCarritoEstilo").removeClass("botonCarritoEstiloBlack")
     $(".logoCarritoBarra").removeClass("logoCarritoBarraDark")
+    $(".modal-content").removeClass("modal-content-oscuro")
     localStorage.setItem("blackMode", "light")
     localStorage.setItem("cardOscura", "light")
     localStorage.setItem("tituloDarkMode", "light")
@@ -63,6 +64,7 @@ $( () =>{
     $(".tituloTienda").addClass("tituloModoOscuro")
     $(".botonCarritoEstilo").addClass("botonCarritoEstiloBlack")
     $(".logoCarritoBarra").addClass("logoCarritoBarraDark")
+    $(".modal-content").addClass("modal-content-oscuro")
     localStorage.setItem("blackMode", "light")
     localStorage.setItem("cardOscura", "light")
     localStorage.setItem("tituloDarkMode", "light")
@@ -72,7 +74,7 @@ $( () =>{
 })
 
                                                     /*CARDS*/
-
+localStorage.setItem('carrito', JSON.stringify([]))
 let divProductos = document.getElementById(`divProductos`)
 let botonCarrito = document.getElementById(`botonCarrito`)
 let modalBody = document.getElementById(`modalBody`)
@@ -83,25 +85,25 @@ fetch('../productos.json')
     .then(datosProductos =>{
         datosProductos.forEach((productoEnArray, indice) => {
             divProductos.innerHTML += `
-            <div class="card" id= "producto${indice}">
-                <img src="../img/producto${indice+1}.jpg" class="card-img-top" alt="...">
+            <div class="card" id= "producto${indice + 1}">
+                <img src="../img/producto${indice + 1}.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h3 class="card-title">${productoEnArray.tipo}</h3>
                         <h4 class="card-text"><b>Modelo:</b> ${productoEnArray.modelo}</h4>
-                        <h5 class="card-text"><b>Precio:</b> ${productoEnArray.precio}</h5>
-                        <button  id="botonAgregar${indice}" class="boton"><img class="carrito" src="../img/carrito.png" alt="carrito"></button>                        
+                        <h5 class="card-text"><b>Precio:</b> $${productoEnArray.precio}</h5>
+                        <button id="botonAgregar${indice+1}" class="boton"><img class="carrito" src="../img/carrito.png" alt="carrito"></button>                        
                     </div>
             </div> 
             `        
         });
         datosProductos.forEach((productoEnArray, indice) =>{
-            document.getElementById(`botonAgregar${indice}`).addEventListener('click', () => {
+            document.getElementById(`botonAgregar${indice+1}`).addEventListener('click', () => {
                 if (productos.find(producto => producto.nombre == productoEnArray.nombre)){
                     let index = productos.findIndex(producto => producto.nombre == productoEnArray.nombre)
                     productos[index].cant++
                     localStorage.setItem('carrito', JSON.stringify(productos))
                 } else {
-                    let producto = new Producto(productoEnArray.tipo, productoEnArray.modelo, productoEnArray.precio, productoEnArray.stock,productoEnArray.img)
+                    let producto = new Producto(productoEnArray.tipo, productoEnArray.modelo, productoEnArray.precio, productoEnArray.stock,productoEnArray.img,productoEnArray.indice)
                     productos.push(producto)
                     localStorage.setItem('carrito', JSON.stringify(productos))
                 }                
@@ -112,48 +114,95 @@ fetch('../productos.json')
 
                                                     /*CARRITO*/
 
-    botonCarrito.addEventListener('click', () => {
-        let productoEnStorage = JSON.parse(localStorage.getItem(`carrito`))
-        productoEnStorage.forEach((productoCarrito, indice) => {
-        modalBody.innerHTML += `
-            <div class="card mb-3" id="productoCarrito${indice} style="max-width: 540px;">
-            <div class="row g-0">
-            <div class="col-md-4">
-                <img src="../img/${productoCarrito.img}" class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                <h5 class="card-title">${productoCarrito.tipo}</h5>
-                <p class="card-text"><b>Modelo:</b>${productoCarrito.modelo}</p>
-                <p class="card-text"><b>Precio:</b>$${productoCarrito.precio}</p>
-                <button id= "botonTacho" class = "btn btn-danger"><img src="../img/tacho.png" alt="tacho"></button>
+    function compraTotal(productosDelStorage) {
+        acumulador = 0;
+        productosDelStorage.forEach((productoCarrito) => {
+            acumulador += productoCarrito.precio * productoCarrito.cant
+        })
+    
+        if(acumulador == 0) {
+            parrafoCompra.innerHTML = ""
+            modalBody.innerHTML = "<p>No hay productos agregados en el carrito </p>" 
+        } else {
+            parrafoCompra.innerHTML = `Importe total $${new Intl.NumberFormat("de-DE").format(acumulador)}`
+        }
+       
+    }
+    
+    function cargarEventosModal(productosDelStorage) {
+    
+        productosDelStorage.forEach((productoCarrito, indice) => {
+            document.getElementById(`botonEliminar${indice}`).addEventListener('click', () => {
+                console.log(`Producto ${productoCarrito.nombre} eliminado`)
+                document.getElementById(`productoCarrito${indice}`).remove()
+                productos.splice(indice, 1)
+                localStorage.setItem('carrito', JSON.stringify(productos))
+                cargarProductosModal(JSON.parse(localStorage.getItem('carrito')))
+            })
+        })
+    
+        productosDelStorage.forEach((productoCarrito, indice) => {
+            document.getElementById(`sum${indice}`).addEventListener('click', () => {
+                console.log()
+                if(productos[indice].cant < productos[indice].stock) {
+                    productos[indice].cant++
+                    localStorage.setItem('carrito', JSON.stringify(productos))
+                    cargarProductosModal(JSON.parse(localStorage.getItem('carrito')))
+                    
+                }
+            })
+        })
+    
+        productosDelStorage.forEach((productoCarrito, indice) => {
+            document.getElementById(`rest${indice}`).addEventListener('click', () => {
+                console.log()
+                if(productos[indice].cant > 1) {
+                    productos[indice].cant--
+                    localStorage.setItem('carrito', JSON.stringify(productos))
+                    cargarProductosModal(JSON.parse(localStorage.getItem('carrito')))
+                }
+            })
+        })
+        
+    }
+    
+    function cargarProductosModal(productosDelStorage) {
+    
+        modalBody.innerHTML = " "  
+        productosDelStorage.forEach((productoCarrito, indice) => {
+            modalBody.innerHTML += `
+                <div class="card mb-3" id ="productoCarrito${indice}" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                        <img src="../img/${productoCarrito.img}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                <div class="col-md-8">
+                    <div class="card-body">    
+                    <h5 class="card-title">${productoCarrito.tipo}</h5>
+                    <p class="card-text"><b>Modelo:</b>${productoCarrito.modelo}</p>
+                    <div class="row">
+                        <p class="card-text"><b>Cantidad:</b> ${productoCarrito.cant}</p>
+                        <button class= "sumar btn btn-outline-secondary" id="sum${indice}"><img src="../img/mas.png" alt="sumar cantidad"></button>
+                        <button class= "restar btn btn-outline-secondary" id="rest${indice}"><img src="../img/resta.png" alt="restar cantidad"></button> 
+                    </div>
+                    <p class="precioTotalCarrito card-text"><b>Precio Total:</b>${new Intl.NumberFormat("de-DE").format(productoCarrito.precio * productoCarrito.cant)}$</p> 
+                    <button class= "btn btn-danger" id="botonEliminar${indice}"><img src="../img/tacho.png" alt="eliminar"></button>
+                </div>
+                </div>
                 </div>
             </div>
-            </div>
-        </div>    
-        ` 
-        })
-        let botonTacho = document.getElementById('botonTacho')
-        botonTacho.addEventListener('click', () => {
-        producto.remove()
-        })        
+        `
     })
-
-// function compraTotal(productosDelStorage) {
-//     acumulador = 0;
-//     productosDelStorage.forEach((productoCarrito) => {
-//         acumulador += productoCarrito.precio * productoCarrito.cant
-//     })
-
-//     if(acumulador == 0) {
-//         parrafoCompra.innerHTML = ""
-//         modalBody.innerHTML = "<p>No hay productos agregados en el carrito </p>" 
-//     } else {
-//         parrafoCompra.innerHTML = `Importe total $${new Intl.NumberFormat("de-DE").format(acumulador)}`
-//     }
-   
-// }   
-
+    cargarEventosModal(productosDelStorage)
+    compraTotal(productosDelStorage)
+    }
+    
+    botonCarrito.addEventListener('click', () => {
+        let productosDelStorage = JSON.parse(localStorage.getItem('carrito'))
+    
+        cargarProductosModal(productosDelStorage)
+        
+    })
 
 
 botonFinalizarCompra.addEventListener(`click`, () => {
